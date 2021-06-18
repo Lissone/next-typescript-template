9ed8f2682d5ae0c6c26d3d2809c4c7459b740ed8
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-18 20:42:27 -0300

    add prettier and integration to eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,5 @@ import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />
 }
