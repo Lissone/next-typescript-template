f42166a4b2bf608796a0cdaa1c16a6f1e5907988
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-18 20:21:53 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+// import styles from '../styles/home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <h1>Hello world</h1>
+    </>
+  )
+}
