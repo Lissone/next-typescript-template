38b931843520c50c691523c3892e3dad6660b3bc
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-18 21:02:34 -0300

    add sass and styled pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-// import styles from '../styles/home.module.css'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -9,7 +9,9 @@ export default function Home() {
         <title>Home</title>
       </Head>
 
-      <h1>Hello world</h1>
+      <main className={styles.container}>
+        <h1>Hello world</h1>
+      </main>
     </>
   )
 }
