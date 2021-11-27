e214a5d3a25be1984c1b0af5341c4a07cf032e5d
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-11-27 12:58:42 -0300

    add eslint import helpers plugin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,10 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
